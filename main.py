5cfb31c509272878ed43994d09d80aff146b14dc
Manuel De Alba <ferman5903@gmail.com>
2024-10-17 17:32:20 -0600

    Add result tables for each instance

--- main.py
+++ main.py
@@ -46,6 +46,10 @@ async def uploadFile(file: UploadFile, modelo: str = Form(...), iteraciones: int
         # Se divide el conjunto de entrenamiento y prueba
         X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size/100, shuffle=True)
 
+        # Unir los conjuntos de entrenamiento y prueba para mostrarlos en la tabla de resultados
+        train = pd.concat([X_train, y_train], axis=1)
+        test = pd.concat([X_test, y_test], axis=1)
+
         # Aplicar el modelo correspondiente
         if modelo == "zero-r":
             # En ZeroR, se predice la clase más frecuente
@@ -60,7 +64,17 @@ async def uploadFile(file: UploadFile, modelo: str = Form(...), iteraciones: int
             recall = recall_score(y_test, y_pred, average="macro")
             f1 = f1_score(y_test, y_pred, average="macro")
 
-            resultados.append({"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1})
+            resultados.append({
+                "accuracy": accuracy,
+                "precision": precision,
+                "recall": recall,
+                "f1": f1,
+                "predicciones": y_pred.tolist(),
+                "columnas_train": train.columns.to_list(),
+                "valores_train": train.values.tolist(),
+                "columnas_test": test.columns.to_list(),
+                "valores_test": test.values.tolist()
+            })
         elif modelo == "one-r":
             # Crear el clasificador OneR
             oner = OneRClassifier()
@@ -71,27 +85,46 @@ async def uploadFile(file: UploadFile, modelo: str = Form(...), iteraciones: int
             # Codificar las variables categóricas
             #? Para regresar los valores a su forma original, se puede usar label_encoder.inverse_transform([...])
             #! Aquí no se puede usar get_dummies porque se pierde la relación entre las variables al modificarse la estructura de la tabla
-            X_train = X_train.apply(label_encoder.fit_transform)
-            X_test = X_test.apply(label_encoder.fit_transform)
-            y_train = label_encoder.fit_transform(y_train)
-            y_test = label_encoder.fit_transform(y_test)
+            X_train_encoded = X_train.apply(label_encoder.fit_transform)
+            X_test_encoded = X_test.apply(label_encoder.fit_transform)
+            y_train_encoded = label_encoder.fit_transform(y_train)
+            y_test_encoded = label_encoder.transform(y_test) # Se usa solo transform para usar los valores ya codificados con y_train
 
             # Entrenar el modelo
-            oner.fit(X_train.values, y_train)
+            oner.fit(X_train_encoded.values, y_train_encoded)
 
             # Predecir
-            y_pred = oner.predict(X_test.values)
+            y_pred_encoded = oner.predict(X_test_encoded.values)
+
+            # Obtener las predicciones en su forma original
+            y_pred_decoded = label_encoder.inverse_transform(y_pred_encoded)
 
             # # Calcular métricas
-            accuracy = accuracy_score(y_test, y_pred)
-            precision = precision_score(y_test, y_pred, average="macro")
-            recall = recall_score(y_test, y_pred, average="macro")
-            f1 = f1_score(y_test, y_pred, average="macro")
+            accuracy = accuracy_score(y_test_encoded, y_pred_encoded)
+            precision = precision_score(y_test_encoded, y_pred_encoded, average="macro")
+            recall = recall_score(y_test_encoded, y_pred_encoded, average="macro")
+            f1 = f1_score(y_test_encoded, y_pred_encoded, average="macro")
 
-            resultados.append({"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1})
+            resultados.append({
+                "accuracy": accuracy,
+                "precision": precision,
+                "recall": recall,
+                "f1": f1,
+                "predicciones": y_pred_decoded.tolist(),
+                "columnas_train": train.columns.to_list(),
+                "valores_train": train.values.tolist(),
+                "columnas_test": test.columns.to_list(),
+                "valores_test": test.values.tolist()
+            })
 
     # Resultados
-    return {"resultados": resultados, "modelo": modelo, "iteraciones": iteraciones, "clase": clase, "train_size": train_size}
+    return {
+        "resultados": resultados,
+        "modelo": modelo,
+        "iteraciones": iteraciones,
+        "clase": clase,
+        "train_size": train_size
+    }
 
 @app.get("/read-file")
 async def readFile():
