dbbbdcf76ce548ba633ffc00f787432f1ff08974
Manuel De Alba <ferman5903@gmail.com>
2024-10-17 12:01:21 -0600

    Add test_size input

--- main.py
+++ main.py
@@ -11,33 +11,40 @@ dataframe = None
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request, modelo="zero-r", iteraciones=1, clase=""):
+async def home(request: Request, modelo="zero-r", iteraciones=1, clase="", test_size=30):
     return templates.TemplateResponse(
         name="index.html",
         request=request,
-        context={"modelo": modelo.lower(), "iteraciones": iteraciones, "clase": clase}
+        context={"modelo": modelo.lower(), "iteraciones": iteraciones, "clase": clase, "test_size": test_size}
     )
 
 @app.post("/upload-file")
-async def uploadFile(file: UploadFile, modelo: str = Form(...), iteraciones: int = Form(...), clase: str = Form(...)):
+async def uploadFile(file: UploadFile, modelo: str = Form(...), iteraciones: int = Form(...), clase: str = Form(...), test_size: int = Form(...)):
     global dataframe
 
+    # Leer el archivo CSV pasado por el usuario
     content = await file.read()
     decoded_content = content.decode("utf-8")
     data = StringIO(decoded_content)
     dataframe = pd.read_csv(data)
-    
+
+    # Separar el dataframe en atributos normales y clase
     X = dataframe.drop(columns=[clase])
     y = dataframe[clase]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # Se repetirá el proceso dependiendo de las iteraciones
+    for i in range(iteraciones):
+        # Se divide el conjunto de entrenamiento y prueba
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=True)
 
-    print("X_train\n", X_train)
-    print("X_test\n", X_test)
-    print("y_train\n", y_train)
-    print("y_test\n", y_test)
+        # Aplicar el modelo correspondiente
+        if modelo == "zero-r":
+            pass
+        elif modelo == "one-r":
+            pass
 
-    return dataframe.to_dict()
+    # Resultados
+    return {"modelo": modelo, "iteraciones": iteraciones, "clase": clase, "test_size": test_size}
 
 @app.get("/read-file")
 async def readFile():
