ee2ea7b5975770c0fee550940730d8e0c4cc30d2
Manuel De Alba <ferman5903@gmail.com>
2024-10-17 11:42:58 -0600

    Split dataset and select target

--- main.py
+++ main.py
@@ -11,23 +11,31 @@ dataframe = None
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request, modelo="zero-r", iteraciones=1):
+async def home(request: Request, modelo="zero-r", iteraciones=1, clase=""):
     return templates.TemplateResponse(
         name="index.html",
         request=request,
-        context={"modelo": modelo.lower(), "iteraciones": iteraciones}
+        context={"modelo": modelo.lower(), "iteraciones": iteraciones, "clase": clase}
     )
 
 @app.post("/upload-file")
-async def uploadFile(file: UploadFile, modelo: str = Form(...), iteraciones: int = Form(...)):
+async def uploadFile(file: UploadFile, modelo: str = Form(...), iteraciones: int = Form(...), clase: str = Form(...)):
     global dataframe
 
     content = await file.read()
     decoded_content = content.decode("utf-8")
     data = StringIO(decoded_content)
     dataframe = pd.read_csv(data)
+    
+    X = dataframe.drop(columns=[clase])
+    y = dataframe[clase]
 
-    X_train, X_test = train_test_split(dataframe, test_size=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    print("X_train\n", X_train)
+    print("X_test\n", X_test)
+    print("y_train\n", y_train)
+    print("y_test\n", y_test)
 
     return dataframe.to_dict()
 
